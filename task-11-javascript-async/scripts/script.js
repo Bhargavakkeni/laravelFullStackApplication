ff5dfc478e35082f179192c08054f6361721ba2a
Akkeni <bhargav.akkeni@appfoster.com>
2024-03-12 12:50:08 +0530

    initial commit

---
+++ task-11-javascript-async/scripts/script.js
@@ -0,0 +1,67 @@
+
+const handleStart = async () => {
+    var userTable = document.getElementById('userTable');
+    try {
+        const response = await fetch('https://gorest.co.in/public/v2/users');
+        if (!response.ok) {
+            throw new Error(`error:${response.status}`);
+        }
+        const data = await response.json();
+        if (data) {
+            //console.log('data inside handlefetch', data);
+            console.log('Fetched data successfuly');
+
+            for (i of data) {
+                userTable.innerHTML += `
+                <tr>
+                    <td>${i.name}</td>
+                    <td>
+                    <span onclick=handleAction(${i.id}) data-bs-toggle="modal" data-bs-target="#Modal">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
+                        </svg>
+                    </span>
+                    </td>
+              </tr>`
+            }
+
+        } else {
+            console.log('While fetching the data');
+            return false;
+        }
+    } catch (error) {
+        console.log('error', error);
+        return false;
+    }
+
+}
+
+handleStart();
+
+async function handleAction(id){
+    var content = document.getElementById('content');
+    content.innerHTML = '';
+    try{
+        const response = await fetch(`https://gorest.co.in/public/v2/users/${id}`);
+
+        if(!response.ok){
+            throw new Error(`error: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if(data){
+            var row = `<tr>`;
+            for(i in data){
+                row += `<td>${data[i]}</td>`;
+            }
+            row += `</tr>`;
+            content.innerHTML += row;
+        } else {
+            throw new Error('Error while fetching the data');
+        }
+    } catch(error) {
+        console.log('Error', error);
+    }
+}
